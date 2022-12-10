47f9f44a65e2248f3ff200503f04aadfdaedb999
Prasann Sitani <prasannsitani000@gmail.com>
2022-12-10 13:25:23 +0530

    style(sidebar): added side bar styling

--- src/App.js
+++ src/App.js
@@ -1,71 +1,11 @@
-import React, { useEffect } from 'react'
-import logo from './logo.svg'
+import React from 'react'
 import './App.css'
-import useFetch from 'react-fetch-hook'
-import { isEmpty } from 'lodash'
+import { Header } from './components'
 
 const App = () => {
-  const { isLoading, data, error } = useFetch('/get-orders', {
-    formatter: response => response.json(),
-  })
-
-  if (isLoading) return <div>Loading...</div>
-  if (error) return <div> {error.status} </div>
-
-  return isEmpty(data) ? (
-    <div> No Data Found </div>
-  ) : (
+  return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-
-        <h1>Menu App</h1>
-        {data.map((item, index) => {
-          return (
-            <div
-              key={index}
-              style={{
-                width: window.innerWidth / 6,
-                background: 'white',
-                boxShadow: '0px 3px 15px rgba(0,0,0,0.2)',
-                padding: '1em',
-                borderRadius: 15,
-                marginBottom: 20,
-                color: 'black',
-                display: 'flex',
-                flexDirection: 'row',
-                justifyContent: 'space-between',
-              }}
-            >
-              <div
-                style={{
-                  display: 'grid',
-                  placeItems: 2,
-                }}
-              >
-                <p style={{ fontSize: '16px' }}>{`${item.ordered_table}`}</p>
-                <p
-                  style={{ fontSize: '16px', color: 'gray' }}
-                >{`As soon as`}</p>
-              </div>
-              <div
-                style={{
-                  alignSelf: 'center',
-                }}
-              >
-                <p
-                  style={{
-                    fontSize: '16px',
-                    color: 'black',
-                    fontWeight: 'bolder',
-                    fontFamily: 'sans-serif',
-                  }}
-                >{`${item.total_amount.displayText}`}</p>
-              </div>
-            </div>
-          )
-        })}
-      </header>
+      <Header />
     </div>
   )
 }
---
+++ src/components/header.js
@@ -0,0 +1,146 @@
+import * as React from 'react'
+import Box from '@mui/material/Box'
+import Drawer from '@mui/material/Drawer'
+import { Avatar } from '@mui/material'
+import Stack from '@mui/material/Stack'
+import { IconButton } from '@mui/material'
+import AcUnitIcon from '@mui/icons-material/AcUnit'
+
+const drawerWidth = 70
+
+const Header = () => {
+  const [mobileOpen, setMobileOpen] = React.useState(false)
+
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen)
+  }
+
+  const drawer = (
+    <div>
+      <Box
+        sx={{
+          padding: '10px',
+          display: 'flex',
+          justifyContent: 'center',
+          background: '#21222D',
+        }}
+      >
+        <Avatar
+          alt="Remy Sharp"
+          src="https://tse1.mm.bing.net/th?id=OIP.TUDe74-_OR6O3P4V-3_FYQHaE7&pid=Api&P=0"
+          sx={{ alignSelf: 'center', border: `2px solid #4054A9` }}
+        />
+      </Box>
+      <Stack
+        sx={{
+          display: 'flex',
+          flex: 1,
+          padding: '10px',
+          justifyContent: 'center',
+          background: '#21222D',
+          height: `calc(${window.screen.height}px - 232px)`,
+        }}
+        spacing={1}
+      >
+        <IconButton size="large" edge="center" color="inherit">
+          <AcUnitIcon sx={{ color: 'white' }} />
+        </IconButton>
+        <IconButton size="large" edge="center" color="inherit">
+          <AcUnitIcon sx={{ color: 'white' }} />
+        </IconButton>
+        <IconButton size="large" edge="center" color="inherit">
+          <AcUnitIcon sx={{ color: 'white' }} />
+        </IconButton>
+        <IconButton size="large" edge="center" color="inherit">
+          <AcUnitIcon sx={{ color: 'white' }} />
+        </IconButton>
+      </Stack>
+    </div>
+  )
+
+  const container = window.document.body
+
+  return (
+    <Box sx={{ display: 'flex' }}>
+      <Box
+        component="nav"
+        sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
+        aria-label="mailbox folders"
+      >
+        {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
+        <Drawer
+          container={container}
+          variant="temporary"
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+          ModalProps={{
+            keepMounted: true, // Better open performance on mobile.
+          }}
+          sx={{
+            display: { xs: 'block', sm: 'none' },
+            '& .MuiDrawer-paper': {
+              boxSizing: 'border-box',
+              width: drawerWidth,
+            },
+          }}
+        >
+          {drawer}
+        </Drawer>
+        <Drawer
+          variant="permanent"
+          sx={{
+            display: { xs: 'none', sm: 'inline-block' },
+            '& .MuiDrawer-paper': {
+              boxSizing: 'border-box',
+              width: drawerWidth,
+            },
+          }}
+          open
+        >
+          {drawer}
+        </Drawer>
+      </Box>
+      {/* <Box
+        component="main"
+        sx={{
+          flexGrow: 1,
+          p: 3,
+          width: { sm: `calc(100% - ${drawerWidth}px)` },
+        }}
+      >
+        <Toolbar />
+        <Typography paragraph>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Rhoncus
+          dolor purus non enim praesent elementum facilisis leo vel. Risus at
+          ultrices mi tempus imperdiet. Semper risus in hendrerit gravida rutrum
+          quisque non tellus. Convallis convallis tellus id interdum velit
+          laoreet id donec ultrices. Odio morbi quis commodo odio aenean sed
+          adipiscing. Amet nisl suscipit adipiscing bibendum est ultricies
+          integer quis. Cursus euismod quis viverra nibh cras. Metus vulputate
+          eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo
+          quis imperdiet massa tincidunt. Cras tincidunt lobortis feugiat
+          vivamus at augue. At augue eget arcu dictum varius duis at consectetur
+          lorem. Velit sed ullamcorper morbi tincidunt. Lorem donec massa sapien
+          faucibus et molestie ac.
+        </Typography>
+        <Typography paragraph>
+          Consequat mauris nunc congue nisi vitae suscipit. Fringilla est
+          ullamcorper eget nulla facilisi etiam dignissim diam. Pulvinar
+          elementum integer enim neque volutpat ac tincidunt. Ornare suspendisse
+          sed nisi lacus sed viverra tellus. Purus sit amet volutpat consequat
+          mauris. Elementum eu facilisis sed odio morbi. Euismod lacinia at quis
+          risus sed vulputate odio. Morbi tincidunt ornare massa eget egestas
+          purus viverra accumsan in. In hendrerit gravida rutrum quisque non
+          tellus orci ac. Pellentesque nec nam aliquam sem et tortor. Habitant
+          morbi tristique senectus et. Adipiscing elit duis tristique
+          sollicitudin nibh sit. Ornare aenean euismod elementum nisi quis
+          eleifend. Commodo viverra maecenas accumsan lacus vel facilisis. Nulla
+          posuere sollicitudin aliquam ultrices sagittis orci a.
+        </Typography>
+      </Box> */}
+    </Box>
+  )
+}
+
+export default Header
