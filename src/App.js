f5f3f8ae0fc51b96f2025f0b320a0e48f37c9256
Prasann Sitani <prasannsitani000@gmail.com>
2022-12-10 17:22:01 +0530

    fix: added browser router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react'
 import './App.css'
 import { Header } from './components'
+import { BrowserRouter } from 'react-router-dom'
+import { ThemeProvider, createTheme } from '@mui/material'
+
+const theme = createTheme()
 
 const App = () => {
   return (
     <div className="App">
-      <Header />
+      <ThemeProvider theme={theme}>
+        <BrowserRouter>
+          <Header />
+        </BrowserRouter>
+      </ThemeProvider>
     </div>
   )
 }
---
+++ src/pages/orders.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { Typography } from '@mui/material'
+
+const Orders = () => {
+  return <Typography> Hello World </Typography>
+}
+
+export default Orders
