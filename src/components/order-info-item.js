c73d6517ef9a36ae7568d86845c06b376a52f704
Prasann Sitani <prasannsitani000@gmail.com>
2022-12-11 15:04:42 +0530

    fix: order info item

--- src/components/order-info-item.js
+++ src/components/order-info-item.js
@@ -1,26 +1,48 @@
-import { Divider, Stack, Typography } from '@mui/material'
+import { Divider, Grid, Stack, Typography, styled, Paper } from '@mui/material'
 import React from 'react'
+
+// const Item = styled(Paper)(({ theme }) => ({
+//   backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+//   ...theme.typography.body2,
+//   padding: theme.spacing(1),
+//   textAlign: 'center',
+//   color: theme.palette.text.secondary,
+// }))
 
 const OrderInfoItem = props => {
   return (
-    <>
-      <Stack direction={{ md: 'row' }} spacing={2} alignItems="center">
-        <img
-          srcSet={`${props.imageUrl}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
-          alt={props.name}
-          loading="lazy"
-          style={{ width: '60px', height: '60px', borderRadius: 10 }}
-        />
-        <Stack
-          sx={{
-            justifyContent: 'space-between',
-          }}
-        >
-          <Typography> Order Info Card </Typography>
-          <Divider />
-        </Stack>
-      </Stack>
-    </>
+    <Stack>
+      <Grid
+        container
+        style={{
+          width: `calc(${window.screen.width}px - 750px)`,
+          textAlign: 'start',
+          alignItems: 'center',
+        }}
+      >
+        <Grid item xs={2}>
+          <img
+            srcSet={`${props.imageUrl}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
+            alt={props.name}
+            loading="lazy"
+            style={{ width: '120px', height: '100px', borderRadius: 10 }}
+          />
+        </Grid>
+        <Grid item xs={3}>
+          <Typography variant="h5">{props.name}</Typography>
+        </Grid>
+        <Grid item xs={2} sx={{ textAlign: 'end' }}>
+          <Typography variant="h5">{`x${props.quantity}`}</Typography>
+        </Grid>
+        <Grid item xs={3} sx={{ textAlign: 'end' }}>
+          <Typography variant="h5">without syrup</Typography>
+        </Grid>
+        <Grid item xs={2} sx={{ textAlign: 'end' }}>
+          <Typography variant="h5">{props.price}</Typography>
+        </Grid>
+      </Grid>
+      <Divider sx={{ width: '87%', alignSelf: 'flex-end' }} />
+    </Stack>
   )
 }
 
--- src/pages/order-info.js
+++ src/pages/order-info.js
@@ -1,24 +1,48 @@
 import React from 'react'
-import { Typography, Stack } from '@mui/material'
+import {
+  Typography,
+  Stack,
+  ListItem,
+  ListItemButton,
+  ListItemText,
+  IconButton,
+  List,
+  Paper,
+} from '@mui/material'
+import CommentIcon from '@mui/icons-material/Comment'
 import { useParams } from 'react-router-dom'
 import { OrderInfoItem } from '../components'
 
 const OrderInfo = props => {
   const { id } = useParams()
 
+  function renderRow(props) {
+    const { index, style } = props
+
+    return (
+      <ListItem style={style} key={index} component="div" disablePadding>
+        <ListItemButton>
+          <ListItemText primary={`Item ${index + 1}`} />
+        </ListItemButton>
+      </ListItem>
+    )
+  }
+
   return (
     <Stack spacing={4}>
-      <Typography variant="h3">Admin Dashboard</Typography>
+      <Typography variant="h2" color="#000" fontWeight="bold">
+        Admin Dashboard
+      </Typography>
       <Stack
         alignItems="flex-start"
         justifyContent="flex-start"
-        sx={{ padding: 10 }}
-        spacing={2}
+        spacing={4}
+        sx={{ padding: 5 }}
       >
-        <Typography variant="h4" color="#2C2B40">
+        <Typography variant="h4" color="#2C2B40" fontWeight="bold">
           Order Info
         </Typography>
-        <Stack spacing={2}>
+        <Stack spacing={4} style={{ maxHeight: 600, overflow: 'auto' }}>
           {[
             {
               id: 1,
@@ -46,10 +70,45 @@ const OrderInfo = props => {
               quantity: 1,
               price: '$14',
             },
+            {
+              id: 4,
+              imageUrl:
+                'https://tse4.mm.bing.net/th?id=OIP.kTvs-fiEdCw7rldk41rhKwHaEo&pid=Api&P=0',
+              name: 'Fruity Pancake',
+              quantity: 2,
+              additional_info: 'without syrup',
+              price: '$12',
+            },
+            {
+              id: 4,
+              imageUrl:
+                'https://tse4.mm.bing.net/th?id=OIP.kTvs-fiEdCw7rldk41rhKwHaEo&pid=Api&P=0',
+              name: 'Fruity Pancake',
+              quantity: 2,
+              additional_info: 'without syrup',
+              price: '$12',
+            },
+            {
+              id: 4,
+              imageUrl:
+                'https://tse4.mm.bing.net/th?id=OIP.kTvs-fiEdCw7rldk41rhKwHaEo&pid=Api&P=0',
+              name: 'Fruity Pancake',
+              quantity: 2,
+              additional_info: 'without syrup',
+              price: '$12',
+            },
           ].map((item, index) => {
             return <OrderInfoItem {...item} key={index} />
           })}
         </Stack>
+        <Typography
+          variant="h3"
+          fontWeight="bold"
+          alignSelf="flex-end"
+          paddingRight="20px"
+        >
+          $5000
+        </Typography>
       </Stack>
     </Stack>
   )
