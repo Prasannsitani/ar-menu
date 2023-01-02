a11dcb9a6acfb00959865158842a9747fc4a406e
Prasann Sitani <prasannsitani000@gmail.com>
2023-01-02 15:48:58 +0530

    chore: update origin url

--- server/server.js
+++ server/server.js
@@ -14,7 +14,10 @@ const app = express()
 
 // header to prevent CORS
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*')
+  res.header(
+    'Access-Control-Allow-Origin',
+    'https://menu-app-admin-luqws.ondigitalocean.app/',
+  )
   res.header(
     'Access-Control-Allow-Headers',
     'Origin, X-Requested-With, Content-Type, Accept',
