55dceb9b511325d179d873acd71f007bb3234d00
Prasann Sitani <prasannsitani000@gmail.com>
2023-01-16 03:25:03 +0530

    fix: disable mobile view

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React from 'react'
 import './App.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import { ThemeProvider, createTheme } from '@mui/material'
+import {
+  ThemeProvider,
+  createTheme,
+  Typography,
+  Box,
+  Stack,
+  Button,
+} from '@mui/material'
 import { Menu } from './pages'
+import { isMobile } from 'react-device-detect'
+import ScreenLockRotationIcon from '@mui/icons-material/ScreenLockRotation'
 
 const theme = createTheme({
   components: {
@@ -26,6 +35,38 @@ const theme = createTheme({
 })
 
 const App = () => {
+  if (isMobile) {
+    return (
+      <div className="App">
+        <ThemeProvider theme={theme}>
+          <Box
+            sx={{
+              display: 'flex',
+              width: `calc(${window.screen.width}px)`,
+              height: `calc(${window.screen.height}px)`,
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}
+          >
+            <Stack alignItems="center" spacing={2}>
+              <ScreenLockRotationIcon sx={{ fontSize: 50, color: 'white' }} />
+              <Typography
+                variant="h5"
+                sx={{
+                  color: 'white',
+                  alignSelf: 'center',
+                }}
+              >
+                {`Use Desktop Screen For Better Experience`}
+              </Typography>
+              <Button onClick={() => window.location.reload()}>Reload</Button>
+            </Stack>
+          </Box>
+        </ThemeProvider>
+      </div>
+    )
+  }
+
   return (
     <div className="App">
       <ThemeProvider theme={theme}>
